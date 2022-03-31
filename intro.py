5062722c328ec97da284a5d5d075e4b68cf2b099
shivam6120 <shivams61btechcse2023@kccitm.edu.in>
2022-03-31 09:01:24 +0530

    X raised to power N

--- intro.py
+++ intro.py
@@ -1,7 +1,15 @@
 # Write a program to input marks of three tests of a student (all integers). Then calculate and print the average of all test marks
 
-m=int(input("enter the maths marks :"))
-eng =int(input("enter the english marks :"))
-physics=int(input("enter the physics marks :"))
-average=(m+eng+physics)/3
-print(average)
+# m=int(input("enter the maths marks :"))
+# eng =int(input("enter the english marks :"))
+# physics=int(input("enter the physics marks :"))
+# average=(m+eng+physics)/3
+# print(average)
+
+
+# You are given two integers: X and N. You have to calculate X raised to power N and print it.
+
+
+x=int(input())
+y=int(input())
+print(x**y)
