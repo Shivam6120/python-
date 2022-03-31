0a850ef1f32968db672db734a050c4365cc46a09
shivam6120 <shivams61btechcse2023@kccitm.edu.in>
2022-03-31 09:05:50 +0530

    rectangular area

--- intro.py
+++ intro.py
@@ -18,9 +18,18 @@
 # You are given first three entries of an arithmetic progression. You have to calculate the common difference and print it
 
 # Write your code here
-a=int(input())
-b=int(input())
-c=int(input())
-d=b-a
-print(d)
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# d=b-a
+# print(d)
 
+# You are given a rectangle in a plane. The coordinates of one of its diagonals are provided to you. You have to print the total area of the rectangle.
+
+# Write your code here
+x1=int(input())
+x2=int(input())
+y1=int(input())
+y2=int(input())
+area=(x1-y1)*(x2-y2)
+print(area)
